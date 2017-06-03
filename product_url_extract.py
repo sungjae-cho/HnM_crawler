88636c2b94858f9fb0c1d1ef4149ce2a8c0a82f8
Sungjae <phoenix2718@gmail.com>
2017-06-03 15:31:06 +0900

    Update

---
+++ product_url_extract.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  2 19:07:03 2017
+
+@author: phoen
+"""
+
+url_women = "http://www.hm.com/us/products/ladies"
+url_men = "http://www.hm.com/us/products/men"
+url_kids = "http://www.hm.com/us/products/kids"
+
