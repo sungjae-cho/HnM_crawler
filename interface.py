9a14262bf8920aed2cb8ed1fa2c3eee30539cf14
Sungjae <phoenix2718@gmail.com>
2017-06-07 19:30:20 +0900

    Pickle data is moved. The related paths are updated. The numbering of IPython Notebooks are added.

--- interface.py
+++ interface.py
@@ -8,7 +8,7 @@ This is a temporary script file.
 import pickle
 
 def import_items_info():
-    pf = open('dict_items_info.p', 'rb')
+    pf = open('pickle_data/dict_items_info.p', 'rb')
     dict_items_info = pickle.load(pf)
     pf.close()
     return dict_items_info
--- product_url_extract.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jun  2 19:07:03 2017
-
-@author: phoen
-"""
-
-
